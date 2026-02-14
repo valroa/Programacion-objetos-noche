7ddac49263953e5ed5b3c60c2de69ae2d54c573a
valentina roa <lvalentinaroa@ucundinamarca.edu.co>
2026-02-13 20:55:10 -0500

    Presentacion

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     system.out.println("Hola mundo");
-    System.outprintln("repaso comando")
+    System.out.println("repaso comando")
+    System.out.println("Laura Valentina Roa Baracaldo")
+    System.out.println("el lenguaje de programacion que he visto y aprendido es c++")
 }
