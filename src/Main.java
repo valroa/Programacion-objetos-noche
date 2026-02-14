f18b8aeeb552f542cd4d58ec3c13fdfebebd1d68
valentina roa <lvalentinaroa@ucundinamarca.edu.co>
2026-02-13 20:51:13 -0500

    salida de consola

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
+    system.out.println("Hola mundo");
+    System.outprintln("repaso comando")
 }
